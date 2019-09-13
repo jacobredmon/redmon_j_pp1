8b8962ae797a262f6dd4d6b7d8dbe6867fa09c23
Jacob Redmon <jrred99@gmail.com>
2019-09-13 15:29:03 -0400

    added comments and javadocs, changed build.xml to allow user to input printflag from command line

--- Application.java
+++ Application.java
@@ -1,43 +1,55 @@
+/**
+ * This class reads through the file given by args[0] and uses the PatternMatching
+ * class to store IP addresses and usernames from a large log file into hashmaps.
+ * Prints off default information if args[1] (the print flag) is 0, the IPs if 1, and the usernames if 2.
+ * @author Jacob Redmon
+ * @version 1.0
+ * Programming Project One
+ * CS322 - Compiler Construction
+ * Spring 2019
+ */
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
 public class Application 
 {
-	
 	public static void main(String[] args) 
 	{
-		long lines = 0;
-		
+		int lines = 0;
+
 		PatternMatching patternMatch = new PatternMatching();
-		
-		try
+
+		try //tries to read the file
 		{
 			Scanner scanner = new Scanner(new File(args[0]));
 			while(scanner.hasNextLine())
 			{
 				patternMatch.PatternMatch(scanner.nextLine());
 				lines++;
-			}
+			}// end whiile
 			scanner.close();
-		}
+		}// end try
 		catch(IOException e)
 		{
 			e.printStackTrace();
-		}
-		
-		if(args[1] == "1")
+		}//end try catcg
+
+		if(args[1].equals("1"))// prints out IP addresses and frequency
 		{
-			patternMatch.getIPAddressesMap().forEach((k,v)->System.out.println(k + " " + v));
-		}
-		if(args[1] == "2")
+			patternMatch.getIPAddressesMap().forEach((k,v)->System.out.println("IP Address: " + k + "; Frequency: " + v));
+			System.out.println();
+		}// end if
+		if(args[1].equals("2"))// prints out usernames and frequency
 		{
-			patternMatch.getUsernamesMap().forEach((k,v)->System.out.println(k + " " + v));
-		}
-		
+			patternMatch.getUsernamesMap().forEach((k,v)->System.out.println("Username: " + k + "; Frequency:  " + v));
+			System.out.println();
+		}// end if
+
 		System.out.println("The file " + args[0] + " contains " + lines + " lines.");
-		System.out.println("There are " + patternMatch.getIPAddressesMapSize() + "unique IP addresses.");
-		System.out.println("There are " + patternMatch.getUsernamesMapSize() + "unique usernames.");
+		System.out.println("There are " + patternMatch.getIPAddressesMapSize() + " unique IP addresses.");
+		System.out.println("There are " + patternMatch.getUsernamesMapSize() + " unique usernames.");
 
 	}// end main
 
--- PatternMatching.java
+++ PatternMatching.java
@@ -1,12 +1,22 @@
+/**
+ * This class initializes two hashmaps for storing IP addresses and usernames
+ * and adds finds appropriate matches for each using regex.
+ * @author Jacob Redmon
+ * @version 1.0
+ * Programming Project One
+ * CS322 - Compiler Construction
+ * Spring 2019
+ */
+
 import java.util.regex.*;
 import java.util.HashMap;
 
 public class PatternMatching 
 {
-	
+
 	private HashMap<String, Integer> IPAddressesMap = new HashMap<>();
 	private HashMap<String, Integer> UsernamesMap = new HashMap<>();
-	
+
 	/*
 	 * Constructor to initialize hashmaps
 	 */
@@ -15,7 +25,7 @@ public class PatternMatching
 		IPAddressesMap = new HashMap<>();
 		UsernamesMap = new HashMap<>();
 	}
-	
+
 	/*
 	 * Accessor to return IPAddressesMap
 	 * @return IPAddressesMap
@@ -24,7 +34,7 @@ public class PatternMatching
 	{
 		return IPAddressesMap;
 	}
-	
+
 	/*
 	 * Accessor to return UsernamesMap
 	 * @return UsernamesMap
@@ -33,7 +43,7 @@ public class PatternMatching
 	{
 		return UsernamesMap;
 	}
-	
+
 	/*
 	 * Returns the number of entries in IPAddressesMap
 	 */
@@ -41,7 +51,7 @@ public class PatternMatching
 	{
 		return IPAddressesMap.size();
 	}
-	
+
 	/*
 	 * Returns the number of entries in UsernamesMap
 	 */
@@ -49,19 +59,19 @@ public class PatternMatching
 	{
 		return UsernamesMap.size();
 	}
-	
+
 	/*
 	 * Searches the string of the log file for any IP addresses and UsernamesMap
 	 * If any are found, adds to the relevant HashMap and increments the value as a frequency
-	 * @param line The String being processed
+	 * @param input The String being processed
 	 */
 	public void PatternMatch(String input) 
 	{
 	    //regex pattern to match only *valid* ip addresses
 	    Matcher ipPattern = Pattern.compile("([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5]\\d)").matcher(input); //pattern for IP addresses
-	    //regex pattern to match usernames in the log file
-	    Matcher usernamePattern = Pattern.compile("username").matcher(input);
-	    
+	    //regex pattern to match usernames in the log file, gets substringed later
+	    Matcher usernamePattern = Pattern.compile("user\\s\\w+").matcher(input);
+
 	    /*
 	     * searches the input string for a match to the IP address regex called 'ipPattern'
 	     * if it's found, adds it to the HashMap and increments the frequency for that IP address by 1, or sets it to 1 if it is a new IP address
@@ -72,12 +82,12 @@ public class PatternMatching
 	    	if(IPAddressesMap.containsKey(match))
 	    	{
 	    		IPAddressesMap.put(match, IPAddressesMap.get(match) + 1);
-	    	} else 
+	    	} else
 	    	{
 	    		IPAddressesMap.put(match, 1);
 	    	}// end if else
 	    }// end while
-	    
+
 	    /*
 	     * searches the input string for a match to the username regex called 'usernamePattern'
 	     * if it's found, adds it to the HashMap and increments the frequency for that username by 1, or sets it to 1 if it is a new username
@@ -85,15 +95,16 @@ public class PatternMatching
 	    while(usernamePattern.find())
 	    {
 	    	String match = usernamePattern.group();
-	    	if(UsernamesMap.containsKey(match))
+		String username = match.substring(5);
+	    	if(UsernamesMap.containsKey(username))
 	    	{
-	    		UsernamesMap.put(match, UsernamesMap.get(match) + 1);
-	    	} else 
+	    		UsernamesMap.put(username, UsernamesMap.get(username) + 1);
+	    	} else
 	    	{
-	    		UsernamesMap.put(match, 1);
+	    		UsernamesMap.put(username, 1);
 	    	}// end if else
 	    }// end while
-	    
+
 	}//end PatternMatch
-	
+
 }// end PatternMatching
